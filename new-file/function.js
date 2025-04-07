47deccb4ca99fe5808ded900da2a41c5863da52d
Prem Choudhary <pk5480524@gmail.com>
2025-04-07 22:47:39 +0530

    new-file

---
+++ new-file/function.js
@@ -0,0 +1,34 @@
+// function isPowerOfTwo(n){
+//     if(n==0){
+//             return false
+//             else{
+//                 while(n%2==0){
+//                     n=n/2
+//                 }
+//             }
+//             if(n==1){
+//                 return true
+//                 else{
+//                     return false
+//                 }
+//             }
+//     }
+// }
+function isPowerOfTwo(n) {
+  if (n == 0) {
+    return false;
+  } else {
+    while (n % 2 == 0) {
+      n = n / 2;
+    }
+  }
+
+  if (n == 1) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(isPowerOfTwo(8));  // true
+console.log(isPowerOfTwo(10)); // false
