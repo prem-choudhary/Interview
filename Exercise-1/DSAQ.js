d12632a6d208811c7e8fb6f7a87878b6da0a3be6
Prem-choudhary <pk5480524@gmail.com>
2025-04-24 12:11:57 +0530

    is-Palindrome

--- Exercise-1/DSAQ.js
+++ Exercise-1/DSAQ.js
@@ -29,3 +29,21 @@ console.log(check_Palindrome("nursesrun"));
 check_Palindrome("nursesrun");
 check_Palindrome("fox");
 
+function isPalindrome(str) {
+  const len = str.length;
+
+  for (let i = 0; i < len / 2; i++) {
+    if (str[i] !== str[len - 1 - i]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+const str1 = "madam";
+const str2 = "nursesrun";
+const str3 = "fox";
+
+console.log(isPalindrome(str1));
+console.log(isPalindrome(str2));
+console.log(isPalindrome(str3));
