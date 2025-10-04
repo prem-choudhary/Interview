f420cdd180d336f7b96965f8012c005302df40ec
Prem-choudhary <pk5480524@gmail.com>
2025-10-04 10:57:00 +0530

    Pattern

--- Calculator/Pattern.js
+++ Calculator/Pattern.js
@@ -28,3 +28,48 @@ for (let i = 1; i <= 5; i++) {
         
  
 */
+// NEW Pattern
+// 
+
+function printPattern(n) {
+  let pattern = "";
+
+  // Upper part
+  for (let i = Math.floor(n / 2); i <= n; i += 2) {
+    // Left spaces
+    for (let j = 1; j < n - i; j += 2) {
+      pattern += " ";
+    }
+    // Left stars
+    for (let j = 1; j <= i; j++) {
+      pattern += "*";
+    }
+    // Middle spaces
+    for (let j = 1; j <= n - i; j++) {
+      pattern += " ";
+    }
+    // Right stars
+    for (let j = 1; j <= i; j++) {
+      pattern += "*";
+    }
+    pattern += "\n";
+  }
+
+  // Lower part
+  for (let i = n; i >= 1; i--) {
+    // Left spaces
+    for (let j = i; j < n; j++) {
+      pattern += " ";
+    }
+    // Stars
+    for (let j = 1; j <= (i * 2) - 1; j++) {
+      pattern += "*";
+    }
+    pattern += "\n";
+  }
+
+  console.log(pattern);
+}
+
+// Run with n = 6
+printPattern(6);
