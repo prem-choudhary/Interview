6461e1b11f78040950b92e25c83bf39463c45be3
Prem-choudhary <pk5480524@gmail.com>
2026-01-10 11:40:37 +0530

    some change

--- Exercise-1/Binary.js
+++ Exercise-1/Binary.js
@@ -17,7 +17,6 @@ function array_binarySearch(narray, delement) {
   } else {
     var arr1 = narray.slice(0, mposition);
     return array_binarySearch(arr1, delement);
-    
   }
 }
 var myArray = [1, 2, 3, 5, 6, 7, 10, 11, 14, 15, 17, 19, 20, 22, 23];
