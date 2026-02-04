35f18311c7dbbdd6a8dd0558e0f43cca2c683049
Prem-choudhary <pk5480524@gmail.com>
2026-02-04 16:14:11 +0530

    code

--- Exercise-1/currentDate.js
+++ Exercise-1/currentDate.js
@@ -1,6 +1,7 @@
 var today = new Date();
 var dd = today.getDate();
 var mm = today.getMonth() + 1;
+console.log(mm)
 var yyyy = today.getFullYear();
 if (dd < 10) {
   dd = "0" + dd;
@@ -9,7 +10,7 @@ if (dd < 10) {
 if (mm < 10) {
   mm = "0" + mm;
 }
-today = mm + "-" + dd + "-" + yyyy;
+today = mm + "-" + dd + "-" + yyyy; 
 console.log(today);
 today = mm + "/" + dd + "/" + yyyy;
 console.log(today);
