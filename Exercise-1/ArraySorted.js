dd8d450cecb22338feb316cf5548ae281cd8d6b5
Prem-choudhary <pk5480524@gmail.com>
2026-04-18 14:11:54 +0530

    Array--Sorted

--- Exercise-1/ArraySorted.js
+++ Exercise-1/ArraySorted.js
@@ -10,34 +10,5 @@ class Solution {
 
     let low = 1;
     let high = n - 2;
-
-    while (low <= high) {
-      let mid = Math.floor((low + high) / 2);
-
-      // Check if mid is the answer
-      if (arr[mid] !== arr[mid - 1] && arr[mid] !== arr[mid + 1]) {
-        return arr[mid];
-      }
-
-      // Check pattern
-      if (
-        (mid % 2 === 0 && arr[mid] === arr[mid + 1]) ||
-        (mid % 2 === 1 && arr[mid] === arr[mid - 1])
-      ) {
-        low = mid + 1; // move right
-      } else {
-        high = mid - 1; // move left
-      }
-    }
-
-    return -1;
   }
 }
-
-// Driver code
-const arr = [1, 1, 2, 2, 3, 3, 4, 5, 5, 6, 6];
-
-const obj = new Solution();
-const ans = obj.singleNonDuplicate(arr);
-
-console.log("The single element is:", ans);
